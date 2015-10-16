b139134af632e994c8b43356ae459fcf6c918ee8
Julio Cesar Garcia Rivera <garcia.rivera.julio@gmail.com>
2015-10-16 00:05:50 -0500

    Manejo de threading sobre el uso seguro de la propiedad location de la ventana principal.

--- frmPrincipal.Designer.vb
+++ frmPrincipal.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmPrincipal
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,136 +20,142 @@ Partial Class frmPrincipal
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.gbMensaje = New System.Windows.Forms.GroupBox()
         Me.txtMensaje = New System.Windows.Forms.TextBox()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
-        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.sbtslblVersion = New System.Windows.Forms.ToolStripStatusLabel()
         Me.tslblVersion = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.gbSoftware = New System.Windows.Forms.GroupBox()
+        Me.txtPumpdemo = New System.Windows.Forms.TextBox()
+        Me.lblPumpdemo = New System.Windows.Forms.Label()
+        Me.txtEnbConfig = New System.Windows.Forms.TextBox()
+        Me.lblEnbConfig = New System.Windows.Forms.Label()
+        Me.txtPsrvr = New System.Windows.Forms.TextBox()
+        Me.lblPsrvr = New System.Windows.Forms.Label()
         Me.niSegundoPlano = New System.Windows.Forms.NotifyIcon(Me.components)
-        Me.GroupBox1.SuspendLayout()
+        Me.gbMensaje.SuspendLayout()
         Me.StatusStrip1.SuspendLayout()
-        Me.GroupBox2.SuspendLayout()
+        Me.gbSoftware.SuspendLayout()
         Me.SuspendLayout()
         '
-        'GroupBox1
+        'gbMensaje
         '
-        Me.GroupBox1.Controls.Add(Me.txtMensaje)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 124)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(358, 76)
-        Me.GroupBox1.TabIndex = 6
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Mensaje: "
+        Me.gbMensaje.Controls.Add(Me.txtMensaje)
+        Me.gbMensaje.Location = New System.Drawing.Point(3, 116)
+        Me.gbMensaje.Name = "gbMensaje"
+        Me.gbMensaje.Size = New System.Drawing.Size(374, 76)
+        Me.gbMensaje.TabIndex = 6
+        Me.gbMensaje.TabStop = False
+        Me.gbMensaje.Text = "[Mensaje:]"
         '
         'txtMensaje
         '
         Me.txtMensaje.Location = New System.Drawing.Point(6, 19)
         Me.txtMensaje.Multiline = True
         Me.txtMensaje.Name = "txtMensaje"
-        Me.txtMensaje.Size = New System.Drawing.Size(346, 51)
+        Me.txtMensaje.ReadOnly = True
+        Me.txtMensaje.Size = New System.Drawing.Size(362, 51)
         Me.txtMensaje.TabIndex = 0
         '
         'StatusStrip1
         '
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.tslblVersion})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 205)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.sbtslblVersion, Me.tslblVersion})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 194)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(385, 22)
+        Me.StatusStrip1.Size = New System.Drawing.Size(381, 22)
         Me.StatusStrip1.TabIndex = 7
         Me.StatusStrip1.Text = "ssEstado"
         '
-        'ToolStripStatusLabel1
+        'sbtslblVersion
         '
-        Me.ToolStripStatusLabel1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(126, 17)
-        Me.ToolStripStatusLabel1.Text = "Versión del Producto:"
-        Me.ToolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.sbtslblVersion.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sbtslblVersion.Name = "sbtslblVersion"
+        Me.sbtslblVersion.Size = New System.Drawing.Size(126, 17)
+        Me.sbtslblVersion.Text = "Versión del Producto:"
+        Me.sbtslblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'tslblVersion
         '
-        Me.tslblVersion.AutoSize = False
         Me.tslblVersion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
         Me.tslblVersion.Name = "tslblVersion"
-        Me.tslblVersion.Size = New System.Drawing.Size(240, 17)
+        Me.tslblVersion.Size = New System.Drawing.Size(16, 17)
         Me.tslblVersion.Text = "..."
-        Me.tslblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.tslblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
-        'GroupBox2
+        'gbSoftware
         '
-        Me.GroupBox2.Controls.Add(Me.TextBox3)
-        Me.GroupBox2.Controls.Add(Me.Label3)
-        Me.GroupBox2.Controls.Add(Me.TextBox2)
-        Me.GroupBox2.Controls.Add(Me.Label2)
-        Me.GroupBox2.Controls.Add(Me.TextBox1)
-        Me.GroupBox2.Controls.Add(Me.Label1)
-        Me.GroupBox2.Location = New System.Drawing.Point(12, 12)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(358, 106)
-        Me.GroupBox2.TabIndex = 8
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Software: Integration Technologies Limited"
+        Me.gbSoftware.Controls.Add(Me.txtPumpdemo)
+        Me.gbSoftware.Controls.Add(Me.lblPumpdemo)
+        Me.gbSoftware.Controls.Add(Me.txtEnbConfig)
+        Me.gbSoftware.Controls.Add(Me.lblEnbConfig)
+        Me.gbSoftware.Controls.Add(Me.txtPsrvr)
+        Me.gbSoftware.Controls.Add(Me.lblPsrvr)
+        Me.gbSoftware.Location = New System.Drawing.Point(3, 4)
+        Me.gbSoftware.Name = "gbSoftware"
+        Me.gbSoftware.Size = New System.Drawing.Size(374, 106)
+        Me.gbSoftware.TabIndex = 8
+        Me.gbSoftware.TabStop = False
+        Me.gbSoftware.Text = "[Software: Integration Technologies Limited]"
         '
-        'TextBox3
+        'txtPumpdemo
         '
-        Me.TextBox3.Location = New System.Drawing.Point(102, 72)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.ReadOnly = True
-        Me.TextBox3.Size = New System.Drawing.Size(238, 20)
-        Me.TextBox3.TabIndex = 11
+        Me.txtPumpdemo.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPumpdemo.ForeColor = System.Drawing.Color.Blue
+        Me.txtPumpdemo.Location = New System.Drawing.Point(102, 72)
+        Me.txtPumpdemo.Name = "txtPumpdemo"
+        Me.txtPumpdemo.ReadOnly = True
+        Me.txtPumpdemo.Size = New System.Drawing.Size(250, 23)
+        Me.txtPumpdemo.TabIndex = 11
         '
-        'Label3
+        'lblPumpdemo
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(19, 75)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(83, 13)
-        Me.Label3.TabIndex = 10
-        Me.Label3.Text = "Pumpdemo.exe:"
+        Me.lblPumpdemo.AutoSize = True
+        Me.lblPumpdemo.Location = New System.Drawing.Point(19, 75)
+        Me.lblPumpdemo.Name = "lblPumpdemo"
+        Me.lblPumpdemo.Size = New System.Drawing.Size(83, 13)
+        Me.lblPumpdemo.TabIndex = 10
+        Me.lblPumpdemo.Text = "Pumpdemo.exe:"
         '
-        'TextBox2
+        'txtEnbConfig
         '
-        Me.TextBox2.Location = New System.Drawing.Point(102, 46)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.ReadOnly = True
-        Me.TextBox2.Size = New System.Drawing.Size(238, 20)
-        Me.TextBox2.TabIndex = 9
+        Me.txtEnbConfig.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEnbConfig.ForeColor = System.Drawing.Color.Blue
+        Me.txtEnbConfig.Location = New System.Drawing.Point(102, 46)
+        Me.txtEnbConfig.Name = "txtEnbConfig"
+        Me.txtEnbConfig.ReadOnly = True
+        Me.txtEnbConfig.Size = New System.Drawing.Size(250, 23)
+        Me.txtEnbConfig.TabIndex = 9
         '
-        'Label2
+        'lblEnbConfig
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(19, 49)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(79, 13)
-        Me.Label2.TabIndex = 8
-        Me.Label2.Text = "EnbConfig.exe:"
+        Me.lblEnbConfig.AutoSize = True
+        Me.lblEnbConfig.Location = New System.Drawing.Point(19, 49)
+        Me.lblEnbConfig.Name = "lblEnbConfig"
+        Me.lblEnbConfig.Size = New System.Drawing.Size(79, 13)
+        Me.lblEnbConfig.TabIndex = 8
+        Me.lblEnbConfig.Text = "EnbConfig.exe:"
         '
-        'TextBox1
+        'txtPsrvr
         '
-        Me.TextBox1.Location = New System.Drawing.Point(102, 20)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.ReadOnly = True
-        Me.TextBox1.Size = New System.Drawing.Size(238, 20)
-        Me.TextBox1.TabIndex = 7
+        Me.txtPsrvr.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPsrvr.ForeColor = System.Drawing.Color.Blue
+        Me.txtPsrvr.Location = New System.Drawing.Point(102, 20)
+        Me.txtPsrvr.Name = "txtPsrvr"
+        Me.txtPsrvr.ReadOnly = True
+        Me.txtPsrvr.Size = New System.Drawing.Size(250, 23)
+        Me.txtPsrvr.TabIndex = 7
         '
-        'Label1
+        'lblPsrvr
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(19, 23)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(54, 13)
-        Me.Label1.TabIndex = 6
-        Me.Label1.Text = "Psrvr.exe:"
+        Me.lblPsrvr.AutoSize = True
+        Me.lblPsrvr.Location = New System.Drawing.Point(19, 23)
+        Me.lblPsrvr.Name = "lblPsrvr"
+        Me.lblPsrvr.Size = New System.Drawing.Size(54, 13)
+        Me.lblPsrvr.TabIndex = 6
+        Me.lblPsrvr.Text = "Psrvr.exe:"
         '
         'niSegundoPlano
         '
@@ -160,35 +166,38 @@ Partial Class frmPrincipal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(385, 227)
-        Me.Controls.Add(Me.GroupBox2)
+        Me.ClientSize = New System.Drawing.Size(381, 216)
+        Me.Controls.Add(Me.gbSoftware)
         Me.Controls.Add(Me.StatusStrip1)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.gbMensaje)
+        Me.DoubleBuffered = True
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "frmPrincipal"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ShowInTaskbar = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "WatchDog: Checksum MD5"
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
+        Me.gbMensaje.ResumeLayout(False)
+        Me.gbMensaje.PerformLayout()
         Me.StatusStrip1.ResumeLayout(False)
         Me.StatusStrip1.PerformLayout()
-        Me.GroupBox2.ResumeLayout(False)
-        Me.GroupBox2.PerformLayout()
+        Me.gbSoftware.ResumeLayout(False)
+        Me.gbSoftware.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents gbMensaje As GroupBox
     Friend WithEvents StatusStrip1 As StatusStrip
-    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents sbtslblVersion As ToolStripStatusLabel
     Friend WithEvents tslblVersion As ToolStripStatusLabel
-    Friend WithEvents GroupBox2 As GroupBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label1 As Label
+    Friend WithEvents gbSoftware As GroupBox
+    Friend WithEvents txtPumpdemo As TextBox
+    Friend WithEvents lblPumpdemo As Label
+    Friend WithEvents txtEnbConfig As TextBox
+    Friend WithEvents lblEnbConfig As Label
+    Friend WithEvents txtPsrvr As TextBox
+    Friend WithEvents lblPsrvr As Label
     Friend WithEvents niSegundoPlano As NotifyIcon
     Friend WithEvents txtMensaje As TextBox
 End Class
--- frmPrincipal.vb
+++ frmPrincipal.vb
@@ -1,6 +1,100 @@
-﻿Public Class frmPrincipal
-    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO
+Imports System.Text
+Imports System.Threading
+Imports System.Security.Cryptography
 
+Public Class frmPrincipal
+    Private hiloVentana As Thread
+    Private Delegate Sub DelUbicarVentana()
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
         tslblVersion.Text = My.Application.Info.Version.ToString
+        hiloVentana = New Thread(New ThreadStart(AddressOf UbicarVentanaSeguro))
+        hiloVentana.Priority = ThreadPriority.Lowest
+        hiloVentana.Name = "UbicacionVentana"
+        hiloVentana.Start()
+    End Sub
+
+    Private Sub ManejodeVersiones()
+
+    End Sub
+
+    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtPsrvr.Text = Md5_Sum("C:\Enabler\psrvr.exe")
+        txtEnbConfig.Text = Md5_Sum("C:\Enabler\EnbConfig.exe")
+        txtPumpdemo.Text = Md5_Sum("C:\Enabler\pumpdemo.exe")
+    End Sub
+
+    Private Sub frmPrincipal_Click(sender As Object, e As EventArgs) Handles MyBase.Click,
+        txtPsrvr.Click, txtPumpdemo.Click, txtEnbConfig.Click, txtMensaje.Click,
+        lblEnbConfig.Click, lblPsrvr.Click, lblPumpdemo.Click, gbMensaje.Click,
+        gbSoftware.Click, tslblVersion.Click, sbtslblVersion.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+
+
+    Private Sub UbicarVentanaSeguro()
+        While True
+
+            If Me.InvokeRequired Then
+                Dim d As New DelUbicarVentana(AddressOf UbicarVentana)
+                Me.Invoke(d)
+            Else
+                Location = New Point(Screen.PrimaryScreen.WorkingArea.Size.Width - Width,
+                                 Screen.PrimaryScreen.WorkingArea.Size.Height - Height)
+            End If
+
+            Thread.Sleep(1000)
+        End While
+    End Sub
+
+    Private Sub UbicarVentana()
+        Location = New Point(Screen.PrimaryScreen.WorkingArea.Size.Width - Width,
+                                 Screen.PrimaryScreen.WorkingArea.Size.Height - Height)
+    End Sub
+
+    ''' <summary>
+    ''' Funcion que me devuelve la verificacion de la integridad de un archivo
+    ''' en este caso, la integridad de la aplicacion: Ubicada en C:\Enabler\psrvr.exe
+    ''' La verificacion de la instalación da como resultado a comparacion:
+    ''' E5B5337FF9AEF32AA19A84AA51CA497C
+    ''' </summary>
+    ''' <param name="dirExe">Direccion del archivo a verificar</param>
+    ''' <returns>Checksum MD5, del servicio de comunicacion: psrvr.exe</returns>
+    ''' <remarks>En caso de no existir el archivo a verificar regresa un: nothing</remarks>
+    Private Function Md5_Sum(ByVal dirExe As String) As String
+        If File.Exists(dirExe) Then
+            Dim md5code As String
+            Dim md5 As MD5CryptoServiceProvider = New MD5CryptoServiceProvider
+
+
+            Dim f As FileStream = New FileStream(dirExe, FileMode.Open, FileAccess.Read, FileShare.Read, 4096)
+            'f = New FileStream(dirExe, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
+            md5.ComputeHash(f)
+
+            Dim ObjFSO As Object = CreateObject("Scripting.FileSystemObject")
+            Dim objFile = ObjFSO.GetFile(dirExe)
+
+            Dim hash As Byte() = md5.Hash
+            Dim buff As StringBuilder = New StringBuilder
+            Dim hashByte As Byte
+            For Each hashByte In hash
+                buff.Append(String.Format("{0:X2}", hashByte))
+            Next
+            md5code = buff.ToString()
+            Return md5code.Trim
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Private Sub frmPrincipal_LocationChanged(sender As Object, e As EventArgs) Handles MyBase.LocationChanged
+        MsgBox(Me.Location.X.ToString & " - " & Me.Location.Y.ToString)
     End Sub
 End Class
