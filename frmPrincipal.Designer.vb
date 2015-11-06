7db72aca4d2e2521a9a12e6b1d6839dd9eca0cb9
Julio Cesar Garcia Rivera <garcia.rivera.julio@gmail.com>
2015-11-05 23:40:57 -0600

    Versión terminada y funcional. La aplicación detecta cada aplicación y compara el checksum MD5 documentado para las pruebas de Hermeticidad. En caso de no cumplir la verificación, la aplicación apaga el sistema en un tiempo programado.

--- frmPrincipal.Designer.vb
+++ frmPrincipal.Designer.vb
@@ -23,11 +23,14 @@ Partial Class frmPrincipal
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmPrincipal))
         Me.gbMensaje = New System.Windows.Forms.GroupBox()
         Me.txtMensaje = New System.Windows.Forms.TextBox()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.sbtslblVersion = New System.Windows.Forms.ToolStripStatusLabel()
         Me.tslblVersion = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.sbtslblApagar = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tslblApagar = New System.Windows.Forms.ToolStripStatusLabel()
         Me.gbSoftware = New System.Windows.Forms.GroupBox()
         Me.txtPumpdemo = New System.Windows.Forms.TextBox()
         Me.lblPumpdemo = New System.Windows.Forms.Label()
@@ -44,28 +47,28 @@ Partial Class frmPrincipal
         'gbMensaje
         '
         Me.gbMensaje.Controls.Add(Me.txtMensaje)
-        Me.gbMensaje.Location = New System.Drawing.Point(3, 116)
+        Me.gbMensaje.Location = New System.Drawing.Point(12, 116)
         Me.gbMensaje.Name = "gbMensaje"
-        Me.gbMensaje.Size = New System.Drawing.Size(374, 76)
+        Me.gbMensaje.Size = New System.Drawing.Size(365, 76)
         Me.gbMensaje.TabIndex = 6
         Me.gbMensaje.TabStop = False
         Me.gbMensaje.Text = "[Mensaje:]"
         '
         'txtMensaje
         '
-        Me.txtMensaje.Location = New System.Drawing.Point(6, 19)
+        Me.txtMensaje.Location = New System.Drawing.Point(22, 19)
         Me.txtMensaje.Multiline = True
         Me.txtMensaje.Name = "txtMensaje"
         Me.txtMensaje.ReadOnly = True
-        Me.txtMensaje.Size = New System.Drawing.Size(362, 51)
+        Me.txtMensaje.Size = New System.Drawing.Size(330, 51)
         Me.txtMensaje.TabIndex = 0
         '
         'StatusStrip1
         '
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.sbtslblVersion, Me.tslblVersion})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 194)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.sbtslblVersion, Me.tslblVersion, Me.sbtslblApagar, Me.tslblApagar})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 195)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(381, 22)
+        Me.StatusStrip1.Size = New System.Drawing.Size(391, 22)
         Me.StatusStrip1.TabIndex = 7
         Me.StatusStrip1.Text = "ssEstado"
         '
@@ -85,6 +88,23 @@ Partial Class frmPrincipal
         Me.tslblVersion.Text = "..."
         Me.tslblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
+        'sbtslblApagar
+        '
+        Me.sbtslblApagar.AutoSize = False
+        Me.sbtslblApagar.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sbtslblApagar.Name = "sbtslblApagar"
+        Me.sbtslblApagar.Size = New System.Drawing.Size(80, 17)
+        Me.sbtslblApagar.Text = "Apagar:"
+        Me.sbtslblApagar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'tslblApagar
+        '
+        Me.tslblApagar.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tslblApagar.ForeColor = System.Drawing.Color.Red
+        Me.tslblApagar.Name = "tslblApagar"
+        Me.tslblApagar.Size = New System.Drawing.Size(16, 17)
+        Me.tslblApagar.Text = "..."
+        '
         'gbSoftware
         '
         Me.gbSoftware.Controls.Add(Me.txtPumpdemo)
@@ -93,9 +113,9 @@ Partial Class frmPrincipal
         Me.gbSoftware.Controls.Add(Me.lblEnbConfig)
         Me.gbSoftware.Controls.Add(Me.txtPsrvr)
         Me.gbSoftware.Controls.Add(Me.lblPsrvr)
-        Me.gbSoftware.Location = New System.Drawing.Point(3, 4)
+        Me.gbSoftware.Location = New System.Drawing.Point(12, 4)
         Me.gbSoftware.Name = "gbSoftware"
-        Me.gbSoftware.Size = New System.Drawing.Size(374, 106)
+        Me.gbSoftware.Size = New System.Drawing.Size(365, 106)
         Me.gbSoftware.TabIndex = 8
         Me.gbSoftware.TabStop = False
         Me.gbSoftware.Text = "[Software: Integration Technologies Limited]"
@@ -159,24 +179,30 @@ Partial Class frmPrincipal
         '
         'niSegundoPlano
         '
-        Me.niSegundoPlano.Text = "NotifyIcon1"
+        Me.niSegundoPlano.Icon = CType(resources.GetObject("niSegundoPlano.Icon"), System.Drawing.Icon)
+        Me.niSegundoPlano.Text = "WatchDog MD5"
         Me.niSegundoPlano.Visible = True
         '
         'frmPrincipal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(381, 216)
+        Me.ClientSize = New System.Drawing.Size(391, 217)
+        Me.ControlBox = False
         Me.Controls.Add(Me.gbSoftware)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.gbMensaje)
         Me.DoubleBuffered = True
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "frmPrincipal"
+        Me.ShowIcon = False
         Me.ShowInTaskbar = False
         Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "WatchDog: Checksum MD5"
+        Me.TopMost = True
         Me.gbMensaje.ResumeLayout(False)
         Me.gbMensaje.PerformLayout()
         Me.StatusStrip1.ResumeLayout(False)
@@ -200,4 +226,6 @@ Partial Class frmPrincipal
     Friend WithEvents lblPsrvr As Label
     Friend WithEvents niSegundoPlano As NotifyIcon
     Friend WithEvents txtMensaje As TextBox
+    Friend WithEvents sbtslblApagar As ToolStripStatusLabel
+    Friend WithEvents tslblApagar As ToolStripStatusLabel
 End Class
--- frmPrincipal.vb
+++ frmPrincipal.vb
@@ -2,16 +2,28 @@
 Imports System.Text
 Imports System.Threading
 Imports System.Security.Cryptography
+Imports System.Diagnostics
 
 Public Class frmPrincipal
+    Private Const PSRVR As String = "C:\Enabler\psrvr.exe"
+    Private Const ENBCONFIG As String = "C:\Enabler\EnbConfig.exe"
+    Private Const PUMPDEMO As String = "C:\Enabler\pumpdemo.exe"
+    Private Const MD5_PSRVR As String = "E5B5337FF9AEF32AA19A84AA51CA497C" '"B0D858A03E6E58888FC29833CAB40268"
+    Private Const MD5_ENBCONFIG As String = "874261BDA5FEA111EC3BF83BE07A282C"
+    Private Const MD5_PUMPDEMO As String = "6F1705C8303FA8B59F9BAD4DA133A289"
+    Private Const SEGUNDOS As Integer = 60
+    Private contadorSegundos As Integer = SEGUNDOS
+    Private Const CMD As Char = "S" '[S = Apagar, R = Apaga y Reinicia el Equipo]
+
     Private hiloVentana As Thread
     Private Delegate Sub DelUbicarVentana()
+    Private Delegate Sub DelMensajeSeguro(ByRef obj As Object, ByVal txt As String)
+    Private ventanaEstado As Boolean = True
 
     Public Sub New()
-
         ' This call is required by the designer.
         InitializeComponent()
-
+        Call UbicarVentana()
         ' Add any initialization after the InitializeComponent() call.
         tslblVersion.Text = My.Application.Info.Version.ToString
         hiloVentana = New Thread(New ThreadStart(AddressOf UbicarVentanaSeguro))
@@ -20,28 +32,12 @@ Public Class frmPrincipal
         hiloVentana.Start()
     End Sub
 
-    Private Sub ManejodeVersiones()
-
-    End Sub
-
     Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        txtPsrvr.Text = Md5_Sum("C:\Enabler\psrvr.exe")
-        txtEnbConfig.Text = Md5_Sum("C:\Enabler\EnbConfig.exe")
-        txtPumpdemo.Text = Md5_Sum("C:\Enabler\pumpdemo.exe")
+        niSegundoPlano.Visible = True
     End Sub
-
-    Private Sub frmPrincipal_Click(sender As Object, e As EventArgs) Handles MyBase.Click,
-        txtPsrvr.Click, txtPumpdemo.Click, txtEnbConfig.Click, txtMensaje.Click,
-        lblEnbConfig.Click, lblPsrvr.Click, lblPumpdemo.Click, gbMensaje.Click,
-        gbSoftware.Click, tslblVersion.Click, sbtslblVersion.Click
-        Me.WindowState = FormWindowState.Minimized
-    End Sub
-
-
 
     Private Sub UbicarVentanaSeguro()
         While True
-
             If Me.InvokeRequired Then
                 Dim d As New DelUbicarVentana(AddressOf UbicarVentana)
                 Me.Invoke(d)
@@ -49,9 +45,81 @@ Public Class frmPrincipal
                 Location = New Point(Screen.PrimaryScreen.WorkingArea.Size.Width - Width,
                                  Screen.PrimaryScreen.WorkingArea.Size.Height - Height)
             End If
-
             Thread.Sleep(1000)
+
+            Call VerificarMD5()
+
         End While
+    End Sub
+
+    Private Sub MensajeSeguro(ByRef obj As Object, ByVal txt As String)
+        If obj.InvokeRequired Then
+            Dim d As New DelMensajeSeguro(AddressOf MensajeSeguro)
+            obj.Invoke(d)
+        Else
+            obj.Text = txt
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Segmento que verifica que el Checksum MD5, coincida con lo calculado
+    ''' y documentado en las pruebas de Hermeticidad, para el software 
+    ''' legalmente relevante, en caso de no concordar, el programa apaga el
+    ''' sistema de comunicación cada "X", segundos declarados en la seccion
+    ''' de Variables al principio del Programa.
+    ''' </summary>
+    Private Sub VerificarMD5()
+1:      Dim del As New DelMensajeSeguro(AddressOf MensajeSeguro)
+2:      Dim banderaPsrvr As Boolean = False
+3:      Dim banderaEnbconfig As Boolean = False
+4:      Dim banderaPumpdemo As Boolean = False
+5:      Dim mensajeCorrecto As Boolean = True
+
+6:      txtPsrvr.Invoke(New DelMensajeSeguro(AddressOf MensajeSeguro),
+                                  New Object() {txtPsrvr, Md5_Sum(PSRVR)})
+7:      txtEnbConfig.Invoke(New DelMensajeSeguro(AddressOf MensajeSeguro),
+                                  New Object() {txtEnbConfig, Md5_Sum(ENBCONFIG)})
+8:      txtPumpdemo.Invoke(New DelMensajeSeguro(AddressOf MensajeSeguro),
+                                  New Object() {txtPumpdemo, Md5_Sum(PUMPDEMO)})
+
+9:      If MD5_PSRVR = Md5_Sum(PSRVR) Then banderaPsrvr = True
+10:     If MD5_ENBCONFIG = Md5_Sum(ENBCONFIG) Then banderaEnbconfig = True
+11:     If MD5_PUMPDEMO = Md5_Sum(PUMPDEMO) Then banderaPumpdemo = True
+
+12:     If banderaPsrvr = False Or banderaEnbconfig = False Or banderaPumpdemo = False Then
+            'En caso de que el textbox donde se colocan los mensajes, cuente con el mismo
+            'mensaje que se va a pasar como parámetro. Esta condición brinca la asignación
+            'así no escribirá el mismo mensaje mas de dos veces
+13:         If txtMensaje.Text <> "Las APLICACIONES fueron CORROMPIDAS!!!" & vbCrLf &
+                                  "PSRVR : " & IIf(banderaPsrvr = False, "CORRUPTO", "NORMAL") &
+                                  ", ENBCONFIG : " & IIf(banderaEnbconfig = False, "CORRUPTO", "NORMAL") &
+                                  ", PUMPDEMO : " & IIf(banderaPumpdemo = False, "CORRUPTO", "NORMAL") &
+                                  ": EL SISTEMA SE APAGARA EN UN INSTANTE" Then
+14:             txtMensaje.Invoke(del, New Object() {txtMensaje,
+                              "Las APLICACIONES fueron CORROMPIDAS!!!" & vbCrLf &
+                              "PSRVR : " & IIf(banderaPsrvr = False, "CORRUPTO", "NORMAL") &
+                              ", ENBCONFIG : " & IIf(banderaEnbconfig = False, "CORRUPTO", "NORMAL") &
+                              ", PUMPDEMO : " & IIf(banderaPumpdemo = False, "CORRUPTO", "NORMAL") &
+                              ": EL SISTEMA SE APAGARA EN UN INSTANTE"})
+15:         End If
+
+16:         tslblApagar.Text = contadorSegundos
+17:         If contadorSegundos > 0 Then
+18:             contadorSegundos -= 1
+19:         Else
+20:             Process.Start("shutdown.exe", " -" & CMD & " -t " & contadorSegundos & " -f")
+21:         End If
+22:         mensajeCorrecto = False
+23:     End If
+
+24:     If mensajeCorrecto Then
+25:         txtMensaje.Invoke(del,
+                                  New Object() {txtMensaje,
+                                  "TODAS LAS APLICACIONES DE COMUNICACION " &
+                                  "FUNCIONAN CORRECTAMENTE!!!"})
+26:         contadorSegundos = SEGUNDOS
+27:         tslblApagar.Text = "..."
+28:     End If
     End Sub
 
     Private Sub UbicarVentana()
@@ -69,32 +137,50 @@ Public Class frmPrincipal
     ''' <returns>Checksum MD5, del servicio de comunicacion: psrvr.exe</returns>
     ''' <remarks>En caso de no existir el archivo a verificar regresa un: nothing</remarks>
     Private Function Md5_Sum(ByVal dirExe As String) As String
-        If File.Exists(dirExe) Then
-            Dim md5code As String
-            Dim md5 As MD5CryptoServiceProvider = New MD5CryptoServiceProvider
-
-
-            Dim f As FileStream = New FileStream(dirExe, FileMode.Open, FileAccess.Read, FileShare.Read, 4096)
-            'f = New FileStream(dirExe, FileMode.Open, FileAccess.Read, FileShare.Read, 8192)
-            md5.ComputeHash(f)
-
-            Dim ObjFSO As Object = CreateObject("Scripting.FileSystemObject")
-            Dim objFile = ObjFSO.GetFile(dirExe)
-
-            Dim hash As Byte() = md5.Hash
-            Dim buff As StringBuilder = New StringBuilder
-            Dim hashByte As Byte
-            For Each hashByte In hash
-                buff.Append(String.Format("{0:X2}", hashByte))
-            Next
-            md5code = buff.ToString()
-            Return md5code.Trim
-        Else
-            Return Nothing
-        End If
+        Try
+1:          If File.Exists(dirExe) Then
+2:              Dim md5code As String
+3:              Dim md5 As MD5CryptoServiceProvider = New MD5CryptoServiceProvider
+4:              Dim f As FileStream = New FileStream(dirExe, FileMode.Open, FileAccess.Read, FileShare.Read, 4096)
+5:              md5.ComputeHash(f)
+6:              Dim ObjFSO As Object = CreateObject("Scripting.FileSystemObject")
+7:              Dim objFile = ObjFSO.GetFile(dirExe)
+8:              Dim hash As Byte() = md5.Hash
+9:              Dim buff As StringBuilder = New StringBuilder
+10:             Dim hashByte As Byte
+11:             For Each hashByte In hash
+12:                 buff.Append(String.Format("{0:X2}", hashByte))
+13:             Next
+14:             md5code = buff.ToString()
+                f.Dispose()
+15:             Return md5code.Trim
+16:         Else
+17:             Return "NO EXISTE LA APLICACION"
+18:         End If
+        Catch ex As Exception
+            'Call manejoErrores(Err.Number, Erl, "MD5_Sum: " & ex.Message.ToString)
+            Return "ERROR"
+        End Try
     End Function
 
-    Private Sub frmPrincipal_LocationChanged(sender As Object, e As EventArgs) Handles MyBase.LocationChanged
-        MsgBox(Me.Location.X.ToString & " - " & Me.Location.Y.ToString)
+    Private Sub niSegundoPlano_Click(sender As Object, e As EventArgs) Handles niSegundoPlano.Click
+        If ventanaEstado Then
+            ventanaEstado = False
+            Me.Hide()
+        Else
+            Me.Show()
+            ventanaEstado = True
+        End If
+    End Sub
+
+    Private Sub frmPrincipal_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        hiloVentana.Abort()
+    End Sub
+
+    Private Sub frmPrincipal_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        e.Cancel = True
+        'niSegundoPlano.Visible = False
+        'niSegundoPlano.Icon.Dispose()
+        'niSegundoPlano.Dispose()
     End Sub
 End Class
